9f4ebb768868f3c3883a65d8f22c4cb649523ff4
anderscodes <anders.j.persson@hotmail.com>
2018-04-11 15:58:44 +0000

    Second test

--- lib/fizzbuzz.rb
+++ lib/fizzbuzz.rb
@@ -2,6 +2,9 @@ class Integer
   def fizzbuzz
     if (self % 3 == 0)
       return 'fizz'
+    elsif (self % 5 == 0)
+    return 'buzz'
+  else
     end
   end
 end
--- spec/fizzbuzz_spec.rb
+++ spec/fizzbuzz_spec.rb
@@ -3,4 +3,7 @@ describe'fizzbuzz'do
   it'returns "fizz" when 3'do
     expect(3.fizzbuzz).to eq 'fizz'
   end
+  it 'return "buzz" when 5'do
+      expect(5.fizzbuzz).to eq 'buzz'
+  end
 end
