090c1eef629d01801e21818fe80f9157cf80cf9b
Charlene Chetcuti <cchetcu3@MGC000003514.warley.ford.com>
2018-04-11 17:01:39 +0100

    Test 3

--- lib/fizzbuzz.rb
+++ lib/fizzbuzz.rb
@@ -1,10 +1,12 @@
 class Integer
   def fizzbuzz
-    if (self % 3 == 0)
+    if (self % 5 == 0) && (self % 3 == 0)
+      return 'fizzbuzz'
+    elsif (self % 3 == 0)
       return 'fizz'
     elsif (self % 5 == 0)
-    return 'buzz'
-  else
+      return 'buzz'
+    else
     end
   end
 end
--- spec/fizzbuzz_spec.rb
+++ spec/fizzbuzz_spec.rb
@@ -4,6 +4,9 @@ describe'fizzbuzz'do
     expect(3.fizzbuzz).to eq 'fizz'
   end
   it 'return "buzz" when 5'do
-      expect(5.fizzbuzz).to eq 'buzz'
+    expect(5.fizzbuzz).to eq 'buzz'
+  end
+  it 'return "fizzbuzz" when divisble by multipels of 5 and 3'do
+    expect(15.fizzbuzz).to eq 'fizzbuzz'
   end
 end
